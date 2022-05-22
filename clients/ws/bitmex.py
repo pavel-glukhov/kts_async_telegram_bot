e926d91b9b71d47782c0bcacb81b8e2a558099ef
pavel-glukhov <glukhov.p@gmail.com>
2022-05-23 03:35:54 +0600

    completed bitmex module

--- clients/ws/bitmex.py
+++ clients/ws/bitmex.py
@@ -1,2 +1,17 @@
+import aiohttp
+import json
+
+
 async def fetch_10() -> list[float]:
-    raise NotImplementedError
+    result = []
+    async with aiohttp.ClientSession() as session:
+        ws = await session.ws_connect('wss://ws.bitmex.com/realtime?subscribe=instrument:XBTUSD')
+        async for msg in ws:
+            data = json.loads(msg.data).get('data')
+            if data:
+                if price := data[0].get('fairPrice'):
+                    result.append(price)
+                if len(result) == 10:
+                    break
+
+    return result
